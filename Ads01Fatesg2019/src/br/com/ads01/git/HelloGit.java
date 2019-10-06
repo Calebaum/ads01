352ccffefd134d57f6927eb898064d09b4cf6c46
Calebe Cabral <calebaum@gmail.com>
2019-10-06 10:45:27 -0300

    Adicionado imprimir "Calebe Cabral"

--- Ads01Fatesg2019/src/br/com/ads01/git/HelloGit.java
+++ Ads01Fatesg2019/src/br/com/ads01/git/HelloGit.java
@@ -11,7 +11,8 @@ public class HelloGit {
 
 	
 	public static void main(String[] args) {
-System.out.println("Hello Git");
+		System.out.println("Hello Git");
+		System.out.println("Calebe Cabral");
 	}
 
 }
