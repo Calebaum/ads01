bba2593d4cd865ea69dd2722d87e59de137afb54
Calebe Cabral <calebaum@gmail.com>
2019-10-06 10:37:26 -0300

    Adicionado arquivos via Upload

---
+++ Ads01Fatesg2019/src/br/com/ads01/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.ads01.git;
+
+/**
+ * @author Calebe
+ * Exemplo integração eclipse com github
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+System.out.println("Hello Git");
+	}
+
+}
